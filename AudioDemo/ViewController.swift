48e7249f2dad7cfddff7ca726aa3953a7a3314fe
Kyle Tolle <kyle@nullsix.com>
2018-10-10 12:37:22 -0400

    Initial Commit

---
+++ AudioDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioDemo
+//
+//  Created by Kyle Tolle on 10/10/18.
+//  Copyright © 2018 Kyle Tolle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
